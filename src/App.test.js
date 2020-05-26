ce7d8cd9208a2d916840f5c1f0c8d20e8889bfc0
German Dario <germangp088@gmail.com>
2020-05-26 12:45:24 -0300

    Implement order process steps.

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,15 @@ import React from 'react';
 import App from './App';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { AppProvider } from "./context";
 
 test('renders BENE element', () => {
-  const { getByText } = render(<App />);
+
+  const { getByText } = render(
+    <AppProvider>
+      <App />
+    </AppProvider>
+  );
 
   const element = getByText(/BENE/i);
   expect(element).toBeInTheDocument();
